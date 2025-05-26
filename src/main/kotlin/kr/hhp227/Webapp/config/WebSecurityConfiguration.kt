9a395212a331d0d9cbcc8bd1e86ad3e33869318e
hhp227 <hong227@naver.com>
2025-05-27 01:45:38 +0900

    2025.05.27 Commit

--- src/main/kotlin/kr/hhp227/Webapp/config/WebSecurityConfiguration.kt
+++ src/main/kotlin/kr/hhp227/Webapp/config/WebSecurityConfiguration.kt
@@ -1,15 +1,23 @@
 package kr.hhp227.Webapp.config
 
 import kr.hhp227.Webapp.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+
 
 @Configuration
 @EnableWebSecurity
@@ -40,9 +48,23 @@ class WebSecurityConfiguration(
         return http.build()
     }
 
+    @Bean
+    fun webSecurityCustomizer() = WebSecurityCustomizer { web ->
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+    }
+
     // 사용자 인증 매니저
     @Bean
     fun authenticationManager(authConfig: AuthenticationConfiguration): AuthenticationManager {
         return authConfig.authenticationManager
+    }
+
+    @Autowired
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        auth
+            .inMemoryAuthentication()
+            .withUser("Admin")
+            .password(passwordEncoder.encode("AdminTest"))
+            .roles("ADMIN")
     }
 }
