8dda88497f4eaa9d8d8c7c0fc4129a51730e74cc
hhp227 <hong227@naver.com>
2025-05-27 01:48:08 +0900

    2025.05.27 Commit

--- src/main/kotlin/kr/hhp227/Webapp/config/WebSecurityConfiguration.kt
+++ src/main/kotlin/kr/hhp227/Webapp/config/WebSecurityConfiguration.kt
@@ -9,15 +9,11 @@ import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
-import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher
-
 
 @Configuration
 @EnableWebSecurity
